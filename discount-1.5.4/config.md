6ac5f3e1d56acd246d7a835501475be879d8af70
rentzsch <jwr.git@redshed.net>
2009-09-13 14:09:12 -0500

    [NEW] MarkdownLive 1.5. Jump to Snow Leopard, Xcode 3.2 and discount 1.3.5.

---
+++ discount-1.5.4/config.md
@@ -0,0 +1,5 @@
+#! /bin/sh
+# script generated Sun Sep 13 13:13:07 CDT 2009 by configure.sh
+
+test -d "$1" || mkdir -p "$1"
+exit 0
--- main.m
+++ main.m
@@ -2,8 +2,8 @@
 //  main.m
 //  MarkdownLive
 //
-//  Created by wolf on 5/21/06.
-//  Copyright __MyCompanyName__ 2006 . All rights reserved.
+//  Created by wolf on 9/13/09.
+//  Copyright 2009 Red Shed Software Company. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
