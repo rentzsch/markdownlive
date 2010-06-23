506db84cd54ef7bcd79cee75a7c15f21ae26c3fc
Joshua B. Griffith <josh.griffith@gmail.com>
2010-06-23 12:29:51 +0800

    Correct duplicated #ifndef token

--- discount_wrappers/markdownWrapper.h
+++ discount_wrappers/markdownWrapper.h
@@ -5,10 +5,10 @@
 
 	***************************************************************************/
 
-#ifndef		_mkdioWrapper_
-#define		_mkdioWrapper_
+#ifndef		_markdownWrapper_
+#define		_markdownWrapper_
 
 int mkd_compile_wrapper(Document *doc, int flags);
 int mkd_document_wrapper(Document *p, char **res);
 
-#endif	//	_mkdioWrapper_
+#endif	//	_markdownWrapper_
