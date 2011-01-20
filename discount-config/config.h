c3823c58ed1a69afade7e35d5828505822229a69
Tim van der Horst <timgvdh@gmail.com>
2011-01-20 11:17:03 +1000

    Upgraded discount to v1.6.1

--- discount-config/config.h
+++ discount-config/config.h
@@ -3,13 +3,11 @@
 
 
 #define OS_DARWIN 1
-#define DWORD	unsigned int
-#define WORD	unsigned short
-#define BYTE	unsigned char
 #define HAVE_PWD_H 1
 #define HAVE_GETPWUID 1
 #define HAVE_SRANDOM 1
 #define INITRNG(x) srandom((unsigned int)x)
+#define HAVE_BZERO 1
 #define HAVE_RANDOM 1
 #define COINTOSS() (random()&1)
 #define HAVE_STRCASECMP 1
