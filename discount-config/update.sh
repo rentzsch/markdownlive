8ded5daa69f389a85092fa09462be19316bdbfd5
Tim van der Horst <timgvdh@gmail.com>
2011-01-31 16:05:22 +1000

    Improved update.sh to handle missing comments section, and to give a more descriptive output

--- discount-config/update.sh
+++ discount-config/update.sh
@@ -1,12 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
-# Run the configure script
-cd ../discount/
+status_msg () {
+	echo -e "\033[1m$1\033[0m"
+}
+
+error_msg () {
+	echo -e "\033[31m$1\033[0m" >&2
+	tput sgr0
+}
+
+status_msg "Running configure.sh..."
+
+cd `dirname $0`/../discount/
 ./configure.sh
 
-# Copy important files
-sed '1,/^ *\*\/ *$/ { d; }' <config.h >../discount-config/config.h
-cp mkdio.h ../discount-config/mkdio.h
+status_msg "Copying important files..."
 
-# Clean up unused files
+if head -n 1 config.h | grep -q "^/\*$"; then
+	# remove generated comments in config.h
+	sed '1,/^ *\*\/ *$/ { d; }' <config.h >../discount-config/config.h && echo 'config.h'
+else
+	cp config.h ../discount-config/config.h && echo 'config.h'
+	error_msg "Can't locate config.h comments!"
+	error_msg "Check the diff before committing (and fix this script if you can)"
+fi
+cp mkdio.h ../discount-config/mkdio.h && echo 'mkdio.h'
+
+status_msg "Clean files from working directory..."
+
 git clean -f
+
+status_msg "Done!"
